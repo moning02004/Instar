b0b9202d43b0fedd0165385be5fdc428a1523a9e
Yu Jeonghoon <moning02004@naver.com>
2020-06-26 16:09:32 +0900

    fix

--- instar/app_main/static/js/modal.js
+++ instar/app_main/static/js/modal.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
     let $bOption = $('.btn-option');
     let $currentModal = null;
+
     $bOption.click(function() {
-        id = $(this).val();
+        let id = $(this).val();
+        console.log(id);
         $currentModal = $('#'+id+'.modal-option')
         $currentModal.css('display', 'block');
     });
@@ -19,7 +21,18 @@ $(document).ready(function() {
         } else if ($(this).val() == 'update') {
             location.href = '/post/' + post + '/update'
         } else if ($(this).val() == 'delete') {
-
+            if (!confirm('정말 삭제하시겠습니까? 되돌릴 수 없습니다.')) return false;
+            $.ajax({
+                url: '/post/' + post + '/delete',
+                method: 'post',
+                success: function(response) {
+                    location.replace('/')
+                },
+                error: function(error) {
+                    alert('에러 발생')
+                },
+                timeout: 2000
+            })
         } else if ($(this).val() == 'cancel') {
             $currentModal.css('display', 'none');
         }
